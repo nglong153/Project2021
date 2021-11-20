fa7aa0e050d0496e6c228810d8c943673957a735
nglong153 <nglong153@gmail.com>
2021-11-20 18:50:23 +0700

    first Commit

---
+++ src/components/choose-location-card/ChooseLocationCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-card-title>City {{cityId}}</v-card-title>
+    <v-card-actions>
+      <v-col cols="4" class="mb-6">
+        <v-combobox @blur="chooseCity" v-model="selectedCity" :items="cities" label="city"> </v-combobox>
+      </v-col>
+      <v-col cols="4" class="mb-6">
+        <v-combobox :items="districts" v-model="selectedDistrict" label="District"> </v-combobox>
+      </v-col>
+      <v-col cols="4" class="mb-6">
+        <v-combobox :items="wards" v-model="selectedWard"  label="Ward"> </v-combobox>
+      </v-col>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      selectedCity : 'Hanoi',
+      selectedDistrict :'Quan Hoang Mai',
+      selectedWard:'Yen So',
+      cities: ['Hanoi', 'HCM', 'Da Nang', 'Bac Giang'],
+      districts: ['Quan Hoang Mai', 'Quan Hoan Kiem', 'Quan Cau Giay'],
+      wards: ['Yen So', 'Tran Phu', 'Linh Nam', 'Hai Ba Trung'],
+    }
+  },
+  props:{
+      cityId  :{
+          type : Number,
+          required:true
+      }
+  },
+  methods:{
+      chooseCity:function(){
+         this.$emit('choosed','city',this.cityId,this.selectedCity)
+      },
+  }
+}
+</script>
---
+++ src/views/pages/real-estate-compare/RealEstateCompare.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <choose-location-card @choosed ="compare" :cityId="1"></choose-location-card>
+      </v-col>
+      <v-col>
+        <choose-location-card :cityId="2"></choose-location-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn color="primary" >Compare </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+        <div>
+            <span>{{compareString}}</span>
+        </div>
+         <v-progress-linear value="500000"></v-progress-linear>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ChooseLocationCard from '@/components/choose-location-card/ChooseLocationCard.vue'
+
+export default {
+  components: {
+    ChooseLocationCard,
+  },
+  data() {
+    return {
+      select: ['Hanoi', 'HCM'],
+      cities: ['Hanoi', 'HCM', 'Da Nang', 'Bac Giang'],
+      districts: ['Quan Hoang Mai', 'Quan Hoan Kiem', 'Quan Cau Giay'],
+      wards: ['Yen So', 'Tran Phu', 'Linh Nam', 'Hai Ba Trung'],
+      firstSelectedLocation : "Yen So, Hoang Mai, Hanoi",
+      secondSelectedLocation : "Tran Phu, Quan Hoang Kiem, Hanoi"
+    }
+  },
+  methods:{
+      compare(typeOfSelect,cityId){
+
+      }
+  },
+  computed:{
+      compareString : function(){
+          return this.firstSelectedLocation + " vs " + this.secondSelectedLocation
+      }
+  }
+}
+</script>
